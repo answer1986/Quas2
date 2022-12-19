3ff342d766279d39b2dd8fafa08a4076f15c29df
Arv00316 <arv00316@hotmail.com>
2022-12-19 08:00:13 -0300

    fabricando pagina formulario

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.arq-contacto')
+@extends('essencials.navbar-top')
+@extends('essencials.navbar')
+
+@section('mapa')
+<br>
+<h2 id="titulo-contacto"><b>Contacto</b></h2>
+<img src="{{asset('image/fotos/mapa.png')}}" id="mapa"  alt="mapa" class="img-fluid">
+@endsection
+
+// has un formulario 
+
+
+
+
+
+
+
+
+@section('formulario')
+
+@endsection
+
+
+@extends('essencials.footer')
