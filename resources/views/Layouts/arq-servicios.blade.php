4d6224b7d3a14ab650415a8089459e1870eaec04
Alvaro Riquelme villa <alvaroriquelmevilla@Alvaros-MacBook-Pro.local>
2025-01-04 16:19:08 -0300

    modificando el menu hamburguesa

--- resources/views/Layouts/arq-servicios.blade.php
+++ resources/views/Layouts/arq-servicios.blade.php
@@ -25,10 +25,9 @@
       @yield('sevicios2')
       @yield('footer')
 
-<!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
- JavaScript Bundle with Popper 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-<script>
+<!--<script>
   function mostrar4(){
     document.getElementById('capacitacion_entrenamiento').style.display = 'block';
     document.getElementById('erp-gestion-de-empresas-1').style.display = 'none';
